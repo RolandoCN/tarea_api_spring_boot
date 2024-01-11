accbbe9dedfee745b4603f63d47301846ec9c4cb
RolandoCN <juan.roland@outllok.es>
2024-01-11 16:16:09 -0500

    primer commit

---
+++ src/main/java/ec/edu/espam/tarea_rolando/TareaRolandoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espam.tarea_rolando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaRolandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaRolandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espam/tarea_rolando/TareaRolandoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espam.tarea_rolando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaRolandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
